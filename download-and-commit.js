5d1c1fc5a750ba2eedd64ab05fb212792369a991
Zgrowth <141380129+Zgrowth@users.noreply.github.com>
2024-04-01 11:22:24 +0800

    Update download-and-commit.js

--- download-and-commit.js
+++ download-and-commit.js
@@ -1,48 +1,92 @@
-// download-and-commit.js
 const fs = require('fs');
 const https = require('https');
 const { execSync } = require('child_process');
+const AV = require("leancloud-storage");
 
-// 假设我们已经获取到了要下载的文件列表 URL
-const fileUrls = [
-    'https://example.com/file1.txt',
-    'https://example.com/file2.txt',
-    // 添加更多 URL...
-];
+// 初始化 LeanCloud 应用
+AV.init({
+  appId: "f2b0Nh5pSJzstBiy3p0iajM2-gzGzoHsz", // 替换为你的 App ID
+  appKey: "MY0v9ZfgAOf5VZGgBP4eiG6e", // 替换为你的 App Key
+  serverURL: "https://cn-n1-console-api.leancloud.cn", // 替换为你的 Server URL
+});
+
 
 // 辅助函数下载文件
 const downloadFile = (url, outputPath) => {
-    return new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(outputPath);
-        https.get(url, (response) => {
-            response.pipe(file);
-            file.on('finish', () => {
-                file.close(resolve);
-            });
-        }).on('error', (err) => {
-            fs.unlink(outputPath); // 删除文件
-            reject(err.message);
-        });
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(outputPath);
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(resolve);
+      });
+    }).on('error', (err) => {
+      fs.unlink(outputPath); // 删除文件
+      reject(err.message);
     });
+  });
 };
 
-// 下载所有文件
-Promise.all(
-    fileUrls.map(url => {
-        // 提取文件名，并定义本地下载路径
-        const fileName = url.split('/').pop();
-        const outputPath = `./downloaded/${fileName}`;
-        return downloadFile(url, outputPath);
+function getFiles() {
+  // 获取文件
+  return new Promise((resolve, reject) => {
+    const query = new AV.Query('_File');
+    query.find().then((nodes) => {
+      const files = nodes.map(node => {
+        return {
+          url: node.get('url'),
+          name: node.get('name')
+        }
+      });
+      resolve(files);
+    }).catch((error) => {
+      reject(error);
+      console.error('获取文件失败', error);
+    });
+  })
+}
+
+async function init() {
+  const fileUrls = await getFiles();
+  // 下载所有文件
+  await Promise.all(
+    fileUrls.map(({ url, name }) => {
+      // 提取文件名，并定义本地下载路径
+      const fileName = url.split('/').pop();
+      const outputPath = `./downloaded/${name || fileName}`;
+      const realUrl = url.replace('http://', 'https://');
+      return downloadFile(realUrl, outputPath);
     })
-)
-.then(() => {
-    // 所有文件下载完成后进行提交
-    execSync('git config --local user.name "GitHub Actions"');
-    execSync('git config --local user.email "actions@github.com"');
-    execSync('git add downloaded/*');
-    execSync('git commit -m "Add downloaded files"');
-    execSync('git push');
-})
-.catch((error) => {
-    console.error('Error downloading files:', error);
-});
+  )
+    .then(() => {
+      // 所有文件下载完成后进行提交
+      execSync('git config --local user.name "Zgrowth"');
+      execSync('git config --local user.email "18296884762@163.com"');
+      execSync('git add downloaded/*');
+      execSync('git commit -m "Add downloaded files"');
+      execSync('git push');
+      deleteClassData();
+    })
+    .catch((error) => {
+      console.error('Error downloading files:', error);
+    });
+}
+
+function deleteClassData() {
+  const query = new AV.Query('_File');
+  query.find().then((results) => {
+    // 创建一个删除操作的数组
+    const deletes = results.map(item => {
+      return AV.Object.createWithoutData('_File', item.id).destroy();
+    });
+
+    // 并发执行所有删除操作
+    return Promise.all(deletes);
+  }).then(() => {
+    console.log('全部数据已被删除。');
+  }).catch((err) => {
+    console.error('删除过程中出现错误:', err);
+  });
+}
+
+init();
